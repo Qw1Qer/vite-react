4afb76a9527cbc780f152dc1e47826001a73a6a0
Qw1Qer <artem.ei90@mail.ru>
2025-10-09 20:34:20 +0700

    v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+/// <reference types="vitest" />
 export default defineConfig({
   plugins: [react()],
+  test: {
+
+    globals: true,
+
+    environment: 'jsdom',
+
+  }
 })
